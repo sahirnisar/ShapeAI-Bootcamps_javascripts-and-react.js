98aad15182d360bbea3b09850e87d750354c1de7
sahirnisar <73066924+sahirnisar@users.noreply.github.com>
2021-06-26 22:21:21 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Info(){
+ return(
+  <div className="note">
+  <h1>Javascript and React.js</h1>
+  <p>This is my first bootcamp of shapeAI that I attented</p>
+  </div>
+ );  
+}
+export default Info; 
